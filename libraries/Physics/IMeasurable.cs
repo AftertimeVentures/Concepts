c8924368937857c50b65c208114c0f675ddd2206
Eduard Malakhov <evm@technobee.co.uk>
2018-02-12 09:00:21 +0300

    Add IMeasurable.

---
+++ libraries/Physics/IMeasurable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aftertime.Concepts.Physics
+{
+    public interface IMeasurable
+    {
+        string ToUnitsString();
+    }
+
+    public interface IMeasurable<TUnits>
+    {
+        string ToUnitsString(TUnits units);
+    }
+}
