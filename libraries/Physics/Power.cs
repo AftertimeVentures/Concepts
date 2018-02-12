5f6b5309fc5847244676e3b680f9d9d3481a57cf
Eduard Malakhov <eduard.malakhov@aftertimeventures.com>
2018-02-12 07:22:47 +0300

    Add Power class and not implemented FromWatts static method.

---
+++ libraries/Physics/Power.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aftertime.Concepts.Physics
+{
+    public partial struct Power
+    {
+        public static Power FromWatts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
