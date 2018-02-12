2abf208d5c3bd94fb078741369c3f08ede8bd43a
Eduard Malakhov <evm@technobee.co.uk>
2018-02-12 08:41:21 +0300

    Add Power_CrossTests.

---
+++ tests/Physics-Tests/Power_CrossTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xunit;
+
+namespace Aftertime.Concepts.Physics
+{
+    public sealed class Power_CrossTests
+    {
+        
+    }
+}
